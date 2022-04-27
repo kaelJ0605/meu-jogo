c1ce63778802547166ca8acb46519db87d8dd5a3
kaelcorrea <jupinha36@gmail.com>
2022-04-27 18:53:20 -0300

    hj eu criei o kleiton e o chão do jogo

--- sketch.js
+++ sketch.js
@@ -1,19 +1,23 @@
 var bgImg;
-
-
+var chao;
+var kleiton,kleitonImg;
 
 function preload(){
   bgImg = loadImage("assets/bg.jpeg")
+  kleitonImg = loadImage("assets/shooter_1.png")
 }
 
 function setup() {
   createCanvas(windowWidth,windowHeight);
-  
+  chao = createSprite(width/2,height-140,width,270);
+  kleiton = createSprite(40,height/2+200,20,60);
+  kleiton.addImage("imagem1",kleitonImg);
+  kleiton.scale = 0.3;
 }
 
 function draw() {
   background(bgImg); 
-
+ 
 
 
   drawSprites();
