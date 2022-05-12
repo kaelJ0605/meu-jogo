97659849fd118157b386eca690f4223078f45fe7
kaelcorrea <jupinha36@gmail.com>
2022-05-12 18:35:40 -0300

    eu fiz o kleiton andar para cima e para baixo com um limite e mudei a animação do kleiton quando ele vai atirar

--- sketch.js
+++ sketch.js
@@ -1,24 +1,42 @@
 var bgImg;
 var chao;
-var kleiton,kleitonImg;
+var kleiton, kleitonImg;
+var kleitonShooter;
 
-function preload(){
-  bgImg = loadImage("assets/bg.jpeg")
-  kleitonImg = loadImage("assets/shooter_1.png")
+function preload() {
+  bgImg = loadImage("assets/bg.jpeg");
+  kleitonImg = loadImage("assets/shooter_1.png");
+  kleitonShooter = loadImage("assets/shooter_3.png")
 }
 
 function setup() {
-  createCanvas(windowWidth,windowHeight);
-  chao = createSprite(width/2,height-140,width,270);
-  kleiton = createSprite(40,height/2+200,20,60);
-  kleiton.addImage("imagem1",kleitonImg);
-  kleiton.scale = 0.3;
+  createCanvas(windowWidth, windowHeight);
+  chao = createSprite(width / 2, height - height / 6, width, height / 3);
+  kleiton = createSprite(100, height / 2 + 200, 20, 60);
+  kleiton.addImage("imagem1", kleitonImg);
+  kleiton.addImage("imagem2", kleitonShooter);
+  kleiton.scale = 0.2;
 }
 
 function draw() {
-  background(bgImg); 
- 
-
-
+  background(bgImg);
+  if (kleiton.y > height - 60) {
+    kleiton.y = height - 60;
+  }
+  if (kleiton.y < height - chao.height){
+    kleiton.y = height - chao.height;
+  }
+  if (keyDown("UP_ARROW")) {
+    kleiton.y = kleiton.y - 30;
+  }
+  if (keyDown("DOWN_ARROW")) {
+    kleiton.y = kleiton.y + 30;
+  }
+  if (keyWentDown("space")){
+    kleiton.changeImage("imagem2");
+  }
+  if (keyWentUp("space")){
+    kleiton.changeImage("imagem1");
+  }
   drawSprites();
 }
